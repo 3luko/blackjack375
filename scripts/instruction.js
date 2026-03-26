ebe8d9d4a048803864c9c4a2180d3b135bfe9cbb
3luko <ethanlukandwa@gmail.com>
2026-03-26 10:49:25 -0500

    Instruction and Leaderboard links

--- scripts/instruction.js
+++ scripts/instruction.js
@@ -1,11 +1,11 @@
 const homeBtn = document.getElementById("home");
 homeBtn.addEventListener("click", () => {
-    window.location.href = "../../index.html";
+    window.location.href = "index.html";
 });
 // Pressing H for Homepage
 window.addEventListener('keydown', event => {
     if (event.key === 'H' || event.key === 'h') {
         event.preventDefault();
-        window.location.href = "../../index.html";
+        window.location.href = "index.html";
     }
 });
