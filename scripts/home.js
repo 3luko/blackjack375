25d950c0a7cbf6aff9916acaab561fe32d08139a
Khue-Vo <vo005642@stthomas.edu>
2026-03-26 08:23:58 -0500

    enter keyboard for homepage

--- scripts/home.js
+++ scripts/home.js
@@ -1,17 +1,27 @@
 //Join Game button logic
-const joinGameBtn = document.getElementById('join');
-joinGameBtn.addEventListener('click', () => {
-    window.location.href = "../../player.html";
-});
-joinGameBtn.addEventListener('click', async () => {
-    const response = await fetch('./scripts/config.json');
-    const data = await response.json();
+async function joinGame () {
+    try {
+        const response = await fetch('./scripts/config.json');
+        const data = await response.json();
     
-    const nameInput = document.getElementById('nameInput').value.trim();
-    let finalUsername = nameInput || data.initialState.defaultUsername;
+        const nameInput = document.getElementById('nameInput').value.trim();
+        let finalUsername = nameInput || data.initialState.defaultUsername;
 
-    if (finalUsername != null) {
-        localStorage.setItem('username', finalUsername);
+        if (finalUsername) {
+            localStorage.setItem('username', finalUsername);
+        }
+
+        window.location.href = "../../player.html";
+    } catch (error) {
+        console.error('Error saving username to localStorage:', error);
+    }
+}
+const joinGameBtn = document.getElementById('join');
+joinGameBtn.addEventListener('click', joinGame);
+window.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        joinGame();
     }
 });
 
